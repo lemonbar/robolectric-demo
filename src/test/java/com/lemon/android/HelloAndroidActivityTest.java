0ff44231313bebaf99a86114b46332069b9888ff
lemon <limengabc@163.com>
2014-09-16 16:17:42 +0800

    first commit

---
+++ src/test/java/com/lemon/android/HelloAndroidActivityTest.java
@@ -0,0 +1,26 @@
+package com.lemon.android;
+
+import android.app.Activity;
+import android.widget.TextView;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by lemon on 14-9-16.
+ */
+@RunWith(RobolectricTestRunner.class)
+public class HelloAndroidActivityTest {
+    @Test
+    public void testcase01() throws Exception {
+
+        Activity activity = Robolectric.buildActivity(HelloAndroidActivity.class).create().get();
+        TextView results = (TextView) activity.findViewById(R.id.textView);
+
+        assertThat(results.getText().toString(), equalTo("Testing Android Rocks!"));
+    }
+}
